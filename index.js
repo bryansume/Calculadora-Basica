b2f0091711637e59ca8f35fc6e71f3e092c33b42
bryansume <brsume@alumnatflorida.es>
2024-12-09 14:39:52 +0100

    Creo función de la operación suma

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ console.log("CALCULADORA");
 console.log("------------");
 const rl = require("readline-sync");
 
-let operacion = rl.question("Que operación deseas realizaR (suma, resta): ");
+let operacion = rl.question("Que operación deseas realizar (suma, resta): ");
+
+function suma(num1, num2) {
+  let resultadoSuma = num1 + num2;
+  return resultadoSuma;
+}
+let suma = suma();
