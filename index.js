3b9298be583940c5607214d174bd457ad8ce79f7
bryansume <brsume@alumnatflorida.es>
2024-12-09 15:40:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,19 +3,22 @@ console.log("------------");
 const rl = require("readline-sync");
 
 let operacion = rl.question("Que operación deseas realizar (suma, resta): ");
-
+let num1 = rl.questionInt("Introduce un número: ");
+let num2 = rl.questionInt("Introduce un número: ");
 function suma(num1, num2) {
-  let resultadoSuma = num1 + num2;
-  return resultadoSuma;
+  let resultado = num1 + num2;
+  return resultado;
 }
-let suma = suma();
+let resultadoSuma = suma(num1, num2);
 
 let resta = function resta(num1, num2) {
-  let resultadoResta = num1 - num2;
-  return resultadoResta;
-};
-
-let multiplicacion = function (num1, num2) {
-  let resultado = num1 * num2;
+  let resultado = num1 - num2;
   return resultado;
 };
+let resultadoResta = resta(num1, num2);
+
+if (operacion === "suma") {
+  console.log(`El resultado de la suma es: ${resultadoSuma}`);
+} else if (operacion === "resta") {
+  console.log(`El resultado de la resta es: ${resultadoResta}`);
+}
